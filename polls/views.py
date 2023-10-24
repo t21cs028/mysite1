29b0372393a95d6a40d970a71737ca2516ea8832
JEON HYODAM <t21cs028@yamanashi.ac.jp>
2023-10-24 13:56:35 +0900

    はじめての Django アプリ作成、その 3

--- polls/views.py
+++ polls/views.py
@@ -23,11 +23,13 @@ def index(request):
 
 # Create your views here.
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    #try:
+     #   question = Question.objects.get(pk=question_id)
+    #except Question.DoesNotExist:
+     #   raise Http404("Question does not exist")
     #return HttpResponse("You're looking at question %s." % question_id)
+    #return render(request, "polls/detail.html", {"question": question})
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
